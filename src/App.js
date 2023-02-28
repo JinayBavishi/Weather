50d0e5dcc4af00e21ec2086dd1c46f5ab1ec154d
JinayBavishi <95083589+JinayBavishi@users.noreply.github.com>
2023-02-28 10:18:38 +0530

    Added apache tinkerpop code

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@ import './App.css';
 import Weathercard from './component/Weathercard0';
 import Temp from "./component/temp"
 import React, { useState, useEffect } from 'react';
+import { DriverRemoteConnection } from 'gremlin';
+const addVertexQuery = `g.addV('person').property('name', 'Alice')`;
+
 
 function App() {
   const [dark, setMode] = useState(false)
+  const connection = new DriverRemoteConnection({
+  traversalSource: 'g',
+  mimeType: 'application/vnd.gremlin-v3.0+json',
+  url: 'wss://<your-graph-server>:8182/gremlin'
+    const executeQuery = async (query) => {
+  const traversal = connection.getTraversal();
+  try {
+    const result = await traversal.V().limit(1).toList();
+    console.log('Result:', result);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+executeQuery(addVertexQuery);
+
+});
+
   return (
     <>
       <div className={dark ? 'App dark-mode' : 'App'}>
